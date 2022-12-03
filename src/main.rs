34bb3bbca1c55f182faad64d98783d50533df9eb
Thomas Hurst <tom@hur.st>
2022-12-03 13:18:06 +0000

    Hack around Infinity/-Infinity

--- src/main.rs
+++ src/main.rs
@@ -95,6 +95,15 @@ fn trim_file(path: &Path) -> Result<usize> {
         std::fs::read_to_string(path)
     }?;
 
+    // Stupid hack to avoid the need for a JSON5 parser
+    // json5 converts these to null for some reason
+    // jsondata doesn't preserve order and this or some other detail seems to cause TI to crash
+    // Also neither do pretty printing, meaning an extra pass with json5format, and even worse performance
+    let data = data
+        .replace("\": -Infinity", "\": \"0a3ff2db148fc58333d206b7ff3b3a80d4c0bcb2072d8ce45a397411\"")
+        .replace("\": Infinity", "\": \"0852cefd0e445e0228605787a763fe79d75bb4ad92c32a78d43b8aed\"")
+        .replace("\": NaN", "\": \"9207845e5abaca58cc1ed24084c9fa7c3640b2dc45f96ee680a72938\"");
+
     let mut data: Value = serde_json::from_str(&data)?;
 
     let trimmed =
@@ -111,6 +120,10 @@ fn trim_file(path: &Path) -> Result<usize> {
         .to_string();
 
     let output = serde_json::to_string_pretty(&data)?;
+    let output = output
+        .replace("\"0a3ff2db148fc58333d206b7ff3b3a80d4c0bcb2072d8ce45a397411\"", "-Infinity")
+        .replace("\"0852cefd0e445e0228605787a763fe79d75bb4ad92c32a78d43b8aed\"", "Infinity")
+        .replace("\"9207845e5abaca58cc1ed24084c9fa7c3640b2dc45f96ee680a72938\"", "NaN");
 
     if gzip {
         let mut encoder = Encoder::new(Vec::new())?;
